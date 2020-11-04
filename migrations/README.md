7ed7a89f97e741ab49854a9b19c2379fc5eb4396
izul <izuleon001>
2020-11-04 10:17:16 +0700

    initial diesel setup
    Co-authored-by: Aditya Kresna <aditya.kresna@outlook.co.id>

--- migrations/README.md
+++
@@ -1,3 +0,0 @@
-# SQL Migration Scripts
-
-This folder will be used by [Diesel Migration CLI](https://github.com/diesel-rs/diesel/tree/master/diesel_migrations).
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 #[global_allocator]
 static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
+mod db;
+
 fn main() {
     println!("Hello, world!");
+    let _ = db::establish_connection();
 }
