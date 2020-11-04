0002374d8c55b517a1be2ab2a69102c30ab56c01
izuleon <izuleonel@gmail.com>
2020-11-04 12:03:41 +0700

    initial diesel setup (#2)
    * initial diesel setup
    
    Co-authored-by: Aditya Kresna <aditya.kresna@outlook.co.id>
    
    * Specify numbering_engine schema in .env
    
    Co-authored-by: izul <izuleon001>
    Co-authored-by: Aditya Kresna <aditya.kresna@outlook.co.id>

---
+++ src/schema.rs
@@ -0,0 +1,65 @@
+table! {
+    laporan (id) {
+        id -> Uuid,
+        created_date -> Timestamptz,
+        updated_date -> Nullable<Timestamptz>,
+        judul -> Varchar,
+        nomor -> Nullable<Varchar>,
+        isi -> Text,
+        status -> Laporan_status,
+        satker_id -> Int4,
+        klasifikasi_id -> Int4,
+        pelapor_id -> Int4,
+        urgensi_id -> Int4,
+        uploader_id -> Int4,
+        updated_by_id -> Nullable<Int4>,
+        jenis_id -> Int4,
+    }
+}
+
+table! {
+    pelapor (id) {
+        id -> Int4,
+        nama -> Varchar,
+        satker_id -> Int4,
+    }
+}
+
+table! {
+    referensi (id) {
+        id -> Int4,
+        nama -> Varchar,
+        kode -> Varchar,
+        deskripsi -> Nullable<Text>,
+    }
+}
+
+table! {
+    satuan_kerja (id) {
+        id -> Int4,
+        nama -> Varchar,
+        kode -> Varchar,
+        deskripsi -> Nullable<Text>,
+    }
+}
+
+table! {
+    user (id) {
+        id -> Int4,
+        email -> Varchar,
+        nama -> Varchar,
+        password -> Varchar,
+    }
+}
+
+joinable!(laporan -> pelapor (pelapor_id));
+joinable!(laporan -> satuan_kerja (satker_id));
+joinable!(pelapor -> satuan_kerja (satker_id));
+
+allow_tables_to_appear_in_same_query!(
+    laporan,
+    pelapor,
+    referensi,
+    satuan_kerja,
+    user,
+);
