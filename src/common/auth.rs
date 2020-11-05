d9e675ca1a906703514432f1e7746964290edf06
Muhammad Bima Adi Prabowo <bima@qti.co.id>
2020-11-06 05:35:16 +0700

    Propose actix project structure

---
+++ src/common/auth.rs
@@ -0,0 +1,7 @@
+
+pub fn validate_jwt_token(token: String) -> bool {
+    // Token validation here
+    println!("{:?}", token);
+
+    true
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
 #[global_allocator]
 static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
-mod db;
+mod api;
+mod common { 
+    pub mod auth;
+    pub mod db;
+} // make it accesible to parent folder
 
-fn main() {
-    println!("Hello, world!");
-    let _ = db::establish_connection();
+use actix_web::{middleware, App,  HttpServer};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let bind = "127.0.0.1:8080";
+
+    HttpServer::new(move || {
+        App::new()
+            .wrap(middleware::Logger::default())
+            .configure(api::laporan::init)
+    })
+    .bind(&bind)?
+    .run()
+    .await
 }
