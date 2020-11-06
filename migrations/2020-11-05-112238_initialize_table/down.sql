f194f5f69df96df787e3b9de119bcddd8488e8d8
yosiasm <yosias@qti.co.id>
2020-11-06 14:36:09 +0700

    Initialize table with migration

--- migrations/2020-11-05-112238_initialize_table/down.sql
+++ migrations/2020-11-05-112238_initialize_table/down.sql
@@ -1,9 +1,9 @@
 -- This file should undo anything in `up.sql`
-DROP TYPE IF EXISTS Laporan_status;
-
 DROP TABLE IF EXISTS public.attachment;
 DROP TABLE IF EXISTS public.laporan;
 DROP TABLE IF EXISTS public.pelapor;
 DROP TABLE IF EXISTS public.referensi;
 DROP TABLE IF EXISTS public.satuan_kerja;
 DROP TABLE IF EXISTS public.user;
+
+DROP TYPE IF EXISTS Laporan_status;
--- migrations/2020-11-05-112238_initialize_table/up.sql
+++ migrations/2020-11-05-112238_initialize_table/up.sql
@@ -2,14 +2,14 @@
 CREATE TYPE Laporan_status AS ENUM ('Draft', 'Terhapus', 'Terkirim & Belum Di Approve', 'Terkirim & Sudah Di Approve');
 
 CREATE TABLE public.attachment (
-    id integer NOT NULL PRIMARY KEY,
+    id serial PRIMARY KEY,
     id_laporan uuid NOT NULL,
     nama_file character varying(128) NOT NULL,
     data bytea NOT NULL
 );
 
 CREATE TABLE public.laporan (
-    id uuid NOT NULL PRIMARY KEY,
+    id uuid PRIMARY KEY,
     created_date timestamp with time zone NOT NULL,
     updated_date timestamp with time zone,
     judul character varying(150) NOT NULL,
@@ -27,27 +27,27 @@ CREATE TABLE public.laporan (
 );
 
 CREATE TABLE public.pelapor (
-    id integer NOT NULL PRIMARY KEY,
+    id serial PRIMARY KEY,
     nama character varying(128) NOT NULL,
     satker_id integer NOT NULL
 );
 
 CREATE TABLE public.referensi (
-    id integer NOT NULL PRIMARY KEY,
+    id serial PRIMARY KEY,
     nama character varying(64) NOT NULL,
     kode character varying(32) NOT NULL,
     deskripsi text
 );
 
 CREATE TABLE public.satuan_kerja (
-    id integer NOT NULL PRIMARY KEY,
+    id serial PRIMARY KEY,
     nama character varying(64) NOT NULL,
     kode character varying(32) NOT NULL,
     deskripsi text
 );
 
 CREATE TABLE public.user (
-    id integer NOT NULL PRIMARY KEY,
+    id serial PRIMARY KEY,
     email character varying(128) NOT NULL,
     nama character varying(64) NOT NULL,
     password character varying(128) NOT NULL
