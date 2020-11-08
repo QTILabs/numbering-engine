9fce6c09e38cabbd3cf037ef3b2b7f16faf252b6
izul <izuleon001>
2020-11-08 20:56:37 +0700

    testing jwt decoder

---
+++ src/api/laporan/jwt_laporan_encoder.rs
@@ -0,0 +1,24 @@
+use serde::{Serialize, Deserialize};
+use jsonwebtoken::{Algorithm, DecodingKey, EncodingKey, Header, TokenData, Validation, decode, encode, errors};
+
+/// Our claims struct, it needs to derive `Serialize` and/or `Deserialize`
+/// Disini masukan field yang akan diterima dalam token apa saja untuk laporan
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Claims {
+    sub: String,
+    company: String,
+    exp: usize,
+    
+}
+pub fn encode_into_jwt(the_struct:Claims) -> Result<String,()>{
+    let mut header = Header::new(Algorithm::HS512);
+    header.kid = Some("blabla".to_owned());
+    let key = b"secret"; 
+    let encoded = encode(&header, &the_struct, &EncodingKey::from_secret(key)).unwrap();
+    Ok(encoded)
+}
+pub fn decode_from_jwt(token:String) -> Result<TokenData<Claims>,errors::Error> {
+    let key = b"secret";
+    let decoded = decode::<Claims>(&token, &DecodingKey::from_secret(key), &Validation::default())?;
+    Ok(decoded)
+}
