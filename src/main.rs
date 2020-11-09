7c516c43469c24606757a6bc71c5d40b09d6fefa
izul <izuleonel@gmail.com>
2020-11-09 10:07:11 +0700

    test gpg

--- src/main.rs
+++ src/main.rs
@@ -13,5 +13,23 @@ use crate::common::jwt_laporan;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+<<<<<<< HEAD
     let config = AppConfig::load_from_env();
     let bind = format!("{}:{}", config.bind_ip, config.bind_port);
+=======
+    let bind = "0.0.0.0:8080";
+    let astruct = api::laporan::Claims {
+        sub : "asdf".to_string(),
+        company : "asdf".to_string(),
+        exp : 10000000100
+    };  
+    let token =api::laporan::encode_into_jwt(astruct).unwrap();
+    println!("{}",token);
+    let decoded = api::laporan::decode_from_jwt(token).unwrap();
+    println!("{:?}",decoded);
+    HttpServer::new(move || App::new().wrap(middleware::Logger::default()).configure(api::laporan::init))
+        .bind(&bind)?
+        .run()
+        .await
+}
+>>>>>>> test gpg
