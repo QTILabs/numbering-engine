e62e81cf4eb6a9da96edae3aab69e0d89b3959a0
Aditya Kresna <aditya.kresna@outlook.co.id>
2020-11-10 10:08:33 +0700

    Refactor skeleton to follow mod pattern

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,18 @@
 #[global_allocator]
 static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
-mod api;
-mod common {
-    pub mod auth;
-    pub mod db;
-} // make it accesible to parent folder
+pub(crate) mod common;
+pub(crate) mod db;
+pub(crate) mod endpoint;
 
+use crate::endpoint::route_handler::init as init_handler;
 use actix_web::{middleware, App, HttpServer};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let bind = "0.0.0.0:8080";
 
-    HttpServer::new(move || App::new().wrap(middleware::Logger::default()).configure(api::laporan::init))
+    HttpServer::new(move || App::new().wrap(middleware::Logger::default()).configure(init_handler))
         .bind(&bind)?
         .run()
         .await
