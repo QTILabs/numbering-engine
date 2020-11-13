080b5576b5e516d806dd3cba06918878d462fcc5
Aditya Kresna <aditya.kresna@outlook.co.id>
2020-11-13 10:07:15 +0700

    Add example of a unit test

--- src/db/stored_procedure.rs
+++ src/db/stored_procedure.rs
@@ -1,10 +1,17 @@
+use crate::common::{IdLaporan, Laporan, LaporanStatus, NomorLaporan};
 use diesel::pg::PgConnection;
 use diesel::prelude::*;
 use dotenv::dotenv;
 use std::env;
 
-pub fn establish_connection() -> PgConnection {
+pub(crate) type ResultInsertLaporan = (IdLaporan, NomorLaporan);
+
+pub(crate) fn establish_connection() -> PgConnection {
     dotenv().ok();
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     PgConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
+}
+
+pub(crate) fn insert_laporan(pg_connection: &PgConnection, laporan: Laporan) -> ResultInsertLaporan {
+    (IdLaporan::new_v4(), "some_nomor".into())
 }
