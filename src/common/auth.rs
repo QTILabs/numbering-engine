538009811419ebe75887bb5db2a03532e2338469
BimaAdi <bimaadi419@gmail.com>
2020-11-14 19:35:49 +0700

    Add get header authorization from API request

--- src/common/auth.rs
+++ src/common/auth.rs
@@ -1,6 +1,13 @@
+use actix_web::HttpRequest;
+
 pub fn validate_jwt_token(token: String) -> bool {
     // Token validation here
     println!("{:?}", token);
 
     true
+}
+
+// get header value from api request
+pub fn get_header_value<'a>(req: &'a HttpRequest, header: &str) -> Option<&'a str> {
+    req.headers().get(header)?.to_str().ok()
 }
