719ace1855be4c8b8009fce6593a5fd497ee0a1d
Alli Sandrana <alli.sandrana@gmail.com>
2020-11-24 14:23:10 +0700

    update urutan in sorting numbering

--- migrations/2020-11-06-091111_laporan_trigger/up.sql
+++ migrations/2020-11-06-091111_laporan_trigger/up.sql
@@ -60,13 +60,13 @@ _year int := EXTRACT(
 );
 _urutan int := sp_laporan_get_nomor_position(NEW.jenis_id,NEW.satker_id,NEW.tanggal_laporan);
 BEGIN NEW.nomor := sp_laporan_get_nomor(
-    _urutan,
+    _urutan - 1,
     sp_laporan_get_kode_satker(NEW.satker_id),
     sp_laporan_get_kode_referensi(NEW.jenis_id),
     NEW.tanggal_laporan
 );
-NEW.urutan := _urutan + 1;
-PERFORM sp_laporan_sort_nomor( _urutan + 1, NEW.jenis_id, NEW.satker_id, NEW.tanggal_laporan);
+NEW.urutan := _urutan;
+PERFORM sp_laporan_sort_nomor( _urutan, NEW.jenis_id, NEW.satker_id, NEW.tanggal_laporan);
 RETURN NEW;
 END $$ LANGUAGE plpgsql;
 
@@ -89,14 +89,14 @@ _urutan int := COALESCE(sp_laporan_get_nomor_position(NEW.jenis_id, NEW.satker_i
 BEGIN 
 
 NEW.nomor := sp_laporan_get_nomor(
-    _urutan,
+    _urutan - 1,
     sp_laporan_get_kode_satker(NEW.satker_id),
     sp_laporan_get_kode_referensi(NEW.jenis_id),
     NEW.tanggal_laporan
 );
-NEW.urutan := _urutan + 1;
+NEW.urutan := _urutan;
 
-PERFORM sp_laporan_sort_nomor(_urutan + 1, NEW.jenis_id, NEW.satker_id, NEW.tanggal_laporan);
+PERFORM sp_laporan_sort_nomor(_urutan, NEW.jenis_id, NEW.satker_id, NEW.tanggal_laporan);
 PERFORM sp_laporan_sort_old_nomor(OLD.urutan - 1, OLD.jenis_id, OLD.satker_id, OLD.tanggal_laporan,OLD.id);
 
 RETURN NEW;
@@ -130,13 +130,13 @@ _year int := EXTRACT(
 );
 _urutan int := COALESCE(sp_laporan_get_nomor_position(NEW.jenis_id, NEW.satker_id, NEW.tanggal_laporan),sp_laporan_count_month(NEW.jenis_id, NEW.satker_id,_month,_year));
 BEGIN NEW.nomor := sp_laporan_get_nomor(
-    _urutan,
+    _urutan - 1,
     sp_laporan_get_kode_satker(NEW.satker_id),
     sp_laporan_get_kode_referensi(NEW.jenis_id),
     NEW.tanggal_laporan
 );
-NEW.urutan := _urutan + 1;
-PERFORM sp_laporan_sort_nomor( _urutan + 1, NEW.jenis_id, NEW.satker_id, NEW.tanggal_laporan);
+NEW.urutan := _urutan;
+PERFORM sp_laporan_sort_nomor( _urutan, NEW.jenis_id, NEW.satker_id, NEW.tanggal_laporan);
 RETURN NEW;
 END $$ LANGUAGE plpgsql;
 
@@ -144,4 +144,3 @@ CREATE TRIGGER tr_laporan_edit_draft_status BEFORE
 UPDATE ON public.laporan FOR EACH ROW
     WHEN (OLD.status = 'draft' AND NEW.status = 'terkirim_sudah_diapprove') EXECUTE PROCEDURE public.sp_laporan_edit_draft_status();
 
-	
