41542134578fc615f892be2c41dc94e7f70947bf
Alli Sandrana <alli.sandrana@gmail.com>
2020-11-25 13:50:06 +0700

    update code formatting for readibility

--- migrations/2020-11-04-070016_sp_laporan/up.sql
+++ migrations/2020-11-04-070016_sp_laporan/up.sql
@@ -52,61 +52,143 @@ END
 $$ LANGUAGE plpgsql;
 
 -- Get Nomor Position
-CREATE OR REPLACE FUNCTION sp_laporan_get_nomor_position(_id_jenis int, _id_satker int, _tanggal_laporan timestampTz)
+CREATE OR REPLACE FUNCTION sp_laporan_get_nomor_position(
+	_id_jenis int, 
+	_id_satker int, 
+	_tanggal_laporan timestampTz)
 RETURNS INTEGER AS $$
 BEGIN
-	RETURN (SELECT urutan FROM laporan WHERE satker_id = _id_satker AND jenis_id = _id_jenis AND EXTRACT(MONTH FROM tanggal_laporan) = EXTRACT(MONTH FROM _tanggal_laporan) AND EXTRACT(YEAR FROM tanggal_laporan) = EXTRACT(YEAR FROM _tanggal_laporan) AND tanggal_laporan >= _tanggal_laporan AND status = 'terkirim_sudah_diapprove' ORDER BY tanggal_laporan ASC LIMIT 1);
+	RETURN (
+		SELECT urutan 
+			FROM laporan 
+			WHERE 
+				satker_id = _id_satker 
+				AND jenis_id = _id_jenis 
+				AND EXTRACT(MONTH FROM tanggal_laporan) = EXTRACT(MONTH FROM _tanggal_laporan) 
+				AND EXTRACT(YEAR FROM tanggal_laporan) = EXTRACT(YEAR FROM _tanggal_laporan) 
+				AND tanggal_laporan >= _tanggal_laporan 
+				AND status = 'terkirim_sudah_diapprove' 
+				ORDER BY tanggal_laporan ASC LIMIT 1
+			);
 END
 $$ LANGUAGE plpgsql;
 
 -- Get Last Draft Nomor
-CREATE OR REPLACE FUNCTION sp_laporan_get_last_draft_nomor(_id_jenis int, _id_satker int, _tanggal_laporan timestampTz)
+CREATE OR REPLACE FUNCTION sp_laporan_get_last_draft_nomor(
+	_id_jenis int, 
+	_id_satker int, 
+	_tanggal_laporan timestampTz)
 RETURNS INTEGER AS $$
 BEGIN
-	RETURN (SELECT urutan FROM laporan WHERE satker_id = _id_satker AND jenis_id = _id_jenis AND EXTRACT(MONTH FROM tanggal_laporan) = EXTRACT(MONTH FROM _tanggal_laporan) AND EXTRACT(YEAR FROM tanggal_laporan) = EXTRACT(YEAR FROM _tanggal_laporan) AND tanggal_laporan <= _tanggal_laporan AND status = 'draft' ORDER BY tanggal_laporan DESC LIMIT 1);
+	RETURN (
+		SELECT urutan 
+			FROM laporan 
+			WHERE 
+				satker_id = _id_satker 
+				AND jenis_id = _id_jenis 
+				AND EXTRACT(MONTH FROM tanggal_laporan) = EXTRACT(MONTH FROM _tanggal_laporan) 
+				AND EXTRACT(YEAR FROM tanggal_laporan) = EXTRACT(YEAR FROM _tanggal_laporan) 
+				AND tanggal_laporan <= _tanggal_laporan 
+				AND status = 'draft' 
+				ORDER BY tanggal_laporan DESC LIMIT 1);
 END
 $$ LANGUAGE plpgsql;
 
 -- Sort Nomor
-CREATE OR REPLACE FUNCTION sp_laporan_sort_nomor(num_count int, _id_jenis int, _id_satker int, _tanggal_laporan timestampTz)
+CREATE OR REPLACE FUNCTION sp_laporan_sort_nomor(
+	num_count int, 
+	_id_jenis int, 
+	_id_satker int, 
+	_tanggal_laporan timestampTz)
 RETURNS void AS $$
 DECLARE	
 	data_laporan record;
 	_urutan int;
 BEGIN
-	FOR data_laporan IN SELECT * FROM laporan WHERE satker_id = _id_satker AND jenis_id = _id_jenis AND EXTRACT(MONTH FROM tanggal_laporan) = EXTRACT(MONTH FROM _tanggal_laporan) AND EXTRACT(YEAR FROM tanggal_laporan) = EXTRACT(YEAR FROM _tanggal_laporan) AND tanggal_laporan >= _tanggal_laporan AND status = 'terkirim_sudah_diapprove' ORDER BY tanggal_laporan ASC LOOP
-		_urutan := num_count + 1;
-		UPDATE laporan SET nomor = sp_laporan_get_nomor(num_count,sp_laporan_get_kode_satker(_id_satker),sp_laporan_get_kode_referensi(_id_jenis),data_laporan.tanggal_laporan), urutan = _urutan WHERE id = data_laporan.id;
-		num_count := _urutan;
+	FOR data_laporan IN SELECT * FROM laporan 
+		WHERE satker_id = _id_satker 
+		AND jenis_id = _id_jenis 
+		AND EXTRACT(MONTH FROM tanggal_laporan) = EXTRACT(MONTH FROM _tanggal_laporan) 
+		AND EXTRACT(YEAR FROM tanggal_laporan) = EXTRACT(YEAR FROM _tanggal_laporan) 
+		AND tanggal_laporan >= _tanggal_laporan 
+		AND status = 'terkirim_sudah_diapprove' 
+		ORDER BY tanggal_laporan ASC LOOP
+			_urutan := num_count + 1;
+			UPDATE laporan 
+				SET nomor = sp_laporan_get_nomor(
+					num_count,
+					sp_laporan_get_kode_satker(_id_satker),
+					sp_laporan_get_kode_referensi(_id_jenis),
+					data_laporan.tanggal_laporan), 
+				urutan = _urutan 
+				WHERE id = data_laporan.id;
+			num_count := _urutan;
 	END LOOP;
 END
 $$ LANGUAGE plpgsql;
 
 --- Sort Old Nomor With Exception from old id (so newly updated laporan wont be updated)
-CREATE OR REPLACE FUNCTION sp_laporan_sort_old_nomor(num_count int, _id_jenis int, _id_satker int, _tanggal_laporan timestampTz, old_id uuid)
+CREATE OR REPLACE FUNCTION sp_laporan_sort_old_nomor(
+	num_count int, 
+	_id_jenis int, 
+	_id_satker int, 
+	_tanggal_laporan timestampTz, 
+	old_id uuid)
 RETURNS void AS $$
 DECLARE	
 	data_laporan record;
 	_urutan int;
 BEGIN
-	FOR data_laporan IN SELECT * FROM laporan WHERE satker_id = _id_satker AND jenis_id = _id_jenis AND EXTRACT(MONTH FROM tanggal_laporan) = EXTRACT(MONTH FROM _tanggal_laporan) AND EXTRACT(YEAR FROM tanggal_laporan) = EXTRACT(YEAR FROM _tanggal_laporan) AND tanggal_laporan >= _tanggal_laporan AND status = 'terkirim_sudah_diapprove' AND id != old_id ORDER BY tanggal_laporan ASC LOOP
-		_urutan := num_count + 1;
-		UPDATE laporan SET nomor = sp_laporan_get_nomor(num_count,sp_laporan_get_kode_satker(_id_satker),sp_laporan_get_kode_referensi(_id_jenis),data_laporan.tanggal_laporan), urutan = _urutan WHERE id = data_laporan.id;
-		num_count := _urutan;
+	FOR data_laporan IN SELECT * FROM laporan 
+		WHERE satker_id = _id_satker 
+		AND jenis_id = _id_jenis 
+		AND EXTRACT(MONTH FROM tanggal_laporan) = EXTRACT(MONTH FROM _tanggal_laporan) 
+		AND EXTRACT(YEAR FROM tanggal_laporan) = EXTRACT(YEAR FROM _tanggal_laporan) 
+		AND tanggal_laporan >= _tanggal_laporan 
+		AND status = 'terkirim_sudah_diapprove' 
+		AND id != old_id 
+		ORDER BY tanggal_laporan ASC LOOP
+			_urutan := num_count + 1;
+			UPDATE laporan 
+				SET nomor = sp_laporan_get_nomor(
+					num_count,
+					sp_laporan_get_kode_satker(_id_satker),
+					sp_laporan_get_kode_referensi(_id_jenis),
+					data_laporan.tanggal_laporan), 
+				urutan = _urutan 
+				WHERE id = data_laporan.id;
+			num_count := _urutan;
 	END LOOP;
 END
 $$ LANGUAGE plpgsql;
 
 --- Re-sorting Nomor Laporan
-CREATE OR REPLACE FUNCTION sp_laporan_re_sort_nomor(_id_jenis int, _id_satker int, _month int, _year int)
+CREATE OR REPLACE FUNCTION sp_laporan_re_sort_nomor(
+	_id_jenis int, 
+	_id_satker int, 
+	_month int, 
+	_year int)
 RETURNS void AS $$
 DECLARE	
 	data_laporan record;
 	num_count int := 0;
 BEGIN
-	FOR data_laporan IN SELECT * FROM laporan WHERE satker_id = _id_satker AND jenis_id = _id_jenis AND EXTRACT(MONTH FROM tanggal_laporan) = _month AND EXTRACT(YEAR FROM tanggal_laporan) = _year AND status = 'terkirim_sudah_diapprove' ORDER BY tanggal_laporan ASC LOOP
-		UPDATE laporan SET nomor = sp_laporan_get_nomor(num_count,sp_laporan_get_kode_satker(_id_satker),sp_laporan_get_kode_referensi(_id_jenis),data_laporan.tanggal_laporan), urutan = num_count + 1 WHERE id = data_laporan.id;
-		num_count := num_count + 1;
+	FOR data_laporan IN SELECT * FROM laporan 
+		WHERE satker_id = _id_satker 
+		AND jenis_id = _id_jenis 
+		AND EXTRACT(MONTH FROM tanggal_laporan) = _month 
+		AND EXTRACT(YEAR FROM tanggal_laporan) = _year 
+		AND status = 'terkirim_sudah_diapprove' 
+		ORDER BY tanggal_laporan ASC LOOP
+			UPDATE laporan 
+				SET nomor = sp_laporan_get_nomor(
+					num_count,
+					sp_laporan_get_kode_satker(_id_satker),
+					sp_laporan_get_kode_referensi(_id_jenis),
+					data_laporan.tanggal_laporan), 
+				urutan = num_count + 1 
+				WHERE id = data_laporan.id;
+			num_count := num_count + 1;
 	END LOOP;
 END
 $$ LANGUAGE plpgsql;
--- migrations/2020-11-06-091111_laporan_trigger/up.sql
+++ migrations/2020-11-06-091111_laporan_trigger/up.sql
@@ -21,7 +21,11 @@ END $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER tr_laporan_insert BEFORE
 INSERT ON public.laporan FOR EACH ROW
-    WHEN (NEW.tanggal_laporan = CURRENT_TIMESTAMP AND NEW.status = 'terkirim_sudah_diapprove') EXECUTE PROCEDURE public.sp_laporan_insert();
+    WHEN (
+        NEW.tanggal_laporan = CURRENT_TIMESTAMP 
+        AND NEW.status = 'terkirim_sudah_diapprove'
+        ) 
+    EXECUTE PROCEDURE public.sp_laporan_insert();
 
 -- Trigger before inserting draft laporan to assign nomor laporan
 CREATE OR REPLACE FUNCTION sp_laporan_insert_draft() RETURNS trigger AS $$
@@ -33,7 +37,10 @@ _year int := EXTRACT(
     YEAR
     FROM NEW.tanggal_laporan
 );
-_urutan int := COALESCE(sp_laporan_get_last_draft_nomor(NEW.jenis_id, NEW.satker_id, NEW.tanggal_laporan),sp_laporan_count_month_draft(NEW.jenis_id, NEW.satker_id,_month,_year));
+_urutan int := COALESCE(
+    sp_laporan_get_last_draft_nomor(NEW.jenis_id, NEW.satker_id, NEW.tanggal_laporan),
+    sp_laporan_count_month_draft(NEW.jenis_id, NEW.satker_id,_month,_year)
+);
 BEGIN NEW.nomor := sp_laporan_get_nomor_draft(
     _urutan,
     sp_laporan_get_kode_satker(NEW.satker_id),
@@ -46,7 +53,8 @@ END $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER tr_laporan_insert_draft BEFORE
 INSERT ON public.laporan FOR EACH ROW
-    WHEN (NEW.status = 'draft') EXECUTE PROCEDURE public.sp_laporan_insert_draft();
+    WHEN (NEW.status = 'draft') 
+        EXECUTE PROCEDURE public.sp_laporan_insert_draft();
 
 --- Trigger before inserting laporan to assign nomor laporan, with backdate tanggal_laporan
 CREATE OR REPLACE FUNCTION sp_laporan_insert_backdate() RETURNS trigger AS $$
@@ -72,7 +80,11 @@ END $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER tr_laporan_insert_backdate BEFORE
 INSERT ON public.laporan FOR EACH ROW
-    WHEN (new.tanggal_laporan < CURRENT_TIMESTAMP AND NEW.status = 'terkirim_sudah_diapprove') EXECUTE PROCEDURE public.sp_laporan_insert_backdate();
+    WHEN (
+        new.tanggal_laporan < CURRENT_TIMESTAMP 
+        AND NEW.status = 'terkirim_sudah_diapprove'
+        ) 
+    EXECUTE PROCEDURE public.sp_laporan_insert_backdate();
 
 -- Edit Laporan Trigger
 
@@ -85,7 +97,10 @@ _year int := EXTRACT(
     YEAR
     FROM NEW.tanggal_laporan
 );
-_urutan int := COALESCE(sp_laporan_get_nomor_position(NEW.jenis_id, NEW.satker_id, NEW.tanggal_laporan),sp_laporan_count_month_edit(NEW.jenis_id, NEW.satker_id,_month,_year,OLD.id) + 1);
+_urutan int := COALESCE(
+    sp_laporan_get_nomor_position(NEW.jenis_id, NEW.satker_id, NEW.tanggal_laporan),
+    sp_laporan_count_month_edit(NEW.jenis_id, NEW.satker_id,_month,_year,OLD.id) + 1
+);
 BEGIN 
 
 NEW.nomor := sp_laporan_get_nomor(
@@ -104,7 +119,13 @@ END $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER tr_laporan_edit BEFORE
 UPDATE ON public.laporan FOR EACH ROW
-    WHEN (OLD.tanggal_laporan IS DISTINCT FROM NEW.tanggal_laporan OR OLD.jenis_id IS DISTINCT FROM NEW.jenis_id OR OLD.satker_id IS DISTINCT FROM NEW.satker_id AND NEW.status = 'terkirim_sudah_diapprove') EXECUTE PROCEDURE public.sp_laporan_edit();
+    WHEN (
+        OLD.tanggal_laporan IS DISTINCT FROM NEW.tanggal_laporan 
+        OR OLD.jenis_id IS DISTINCT FROM NEW.jenis_id 
+        OR OLD.satker_id IS DISTINCT FROM NEW.satker_id 
+        AND NEW.status = 'terkirim_sudah_diapprove'
+        ) 
+    EXECUTE PROCEDURE public.sp_laporan_edit();
 
 --- Add or update updated_date when updating table
 
@@ -128,7 +149,10 @@ _year int := EXTRACT(
     YEAR
     FROM NEW.tanggal_laporan
 );
-_urutan int := COALESCE(sp_laporan_get_nomor_position(NEW.jenis_id, NEW.satker_id, NEW.tanggal_laporan),sp_laporan_count_month(NEW.jenis_id, NEW.satker_id,_month,_year) + 1);
+_urutan int := COALESCE(
+    sp_laporan_get_nomor_position(NEW.jenis_id, NEW.satker_id, NEW.tanggal_laporan),
+    sp_laporan_count_month(NEW.jenis_id, NEW.satker_id,_month,_year) + 1
+);
 BEGIN NEW.nomor := sp_laporan_get_nomor(
     _urutan - 1,
     sp_laporan_get_kode_satker(NEW.satker_id),
@@ -142,7 +166,8 @@ END $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER tr_laporan_edit_draft_status BEFORE
 UPDATE ON public.laporan FOR EACH ROW
-    WHEN (OLD.status = 'draft' AND NEW.status = 'terkirim_sudah_diapprove') EXECUTE PROCEDURE public.sp_laporan_edit_draft_status();
+    WHEN (OLD.status = 'draft' AND NEW.status = 'terkirim_sudah_diapprove') 
+        EXECUTE PROCEDURE public.sp_laporan_edit_draft_status();
 
 
 --- Trigger Edit Laporan Status Back to Draft
@@ -155,7 +180,10 @@ _year int := EXTRACT(
     YEAR
     FROM NEW.tanggal_laporan
 );
-_urutan int := COALESCE(sp_laporan_get_last_draft_nomor(NEW.jenis_id, NEW.satker_id, NEW.tanggal_laporan),sp_laporan_count_month_draft(NEW.jenis_id, NEW.satker_id,_month,_year));
+_urutan int := COALESCE(
+    sp_laporan_get_last_draft_nomor(NEW.jenis_id, NEW.satker_id, NEW.tanggal_laporan),
+    sp_laporan_count_month_draft(NEW.jenis_id, NEW.satker_id,_month,_year)
+);
 BEGIN NEW.nomor := sp_laporan_get_nomor_draft(
     _urutan,
     sp_laporan_get_kode_satker(NEW.satker_id),
@@ -169,7 +197,8 @@ END $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER tr_laporan_edit_back_to_draft BEFORE
 UPDATE ON public.laporan FOR EACH ROW
-    WHEN (NEW.status = 'draft') EXECUTE PROCEDURE public.sp_laporan_edit_back_to_draft();
+    WHEN (NEW.status = 'draft') 
+        EXECUTE PROCEDURE public.sp_laporan_edit_back_to_draft();
 
 --- Trigger Edit Laporan Status to terhapus (soft delete laporan)
 CREATE OR REPLACE FUNCTION sp_laporan_delete() RETURNS trigger AS $$
@@ -182,7 +211,8 @@ END $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER tr_laporan_delete BEFORE
 UPDATE ON public.laporan FOR EACH ROW
-    WHEN (NEW.status = 'terhapus' AND OLD.status = 'terkirim_sudah_diapprove') EXECUTE PROCEDURE public.sp_laporan_delete();
+    WHEN (NEW.status = 'terhapus' AND OLD.status = 'terkirim_sudah_diapprove') 
+        EXECUTE PROCEDURE public.sp_laporan_delete();
 
 
 --- Trigger Delete Laporan (HARD DELETE laporan)
@@ -194,5 +224,6 @@ END $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER tr_laporan_hard_delete BEFORE
 DELETE ON public.laporan FOR EACH ROW
-   WHEN (OLD.status = 'terkirim_sudah_diapprove') EXECUTE PROCEDURE public.sp_laporan_hard_delete();
+    WHEN (OLD.status = 'terkirim_sudah_diapprove') 
+        EXECUTE PROCEDURE public.sp_laporan_hard_delete();
 
